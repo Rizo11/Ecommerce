9392266217bb7ce54f6e5ad40fa01de334cc7198
Rizo11 <20455251@fhda.edu>
2022-02-28 17:18:18 +0500

    migrations added

---
+++ Ecommerce/Server/Migrations/20220228065646_1stcreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Server.Migrations
+{
+    public partial class _1stcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Ecommerce/Shared/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.Shared
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18, 2")]
+        public decimal Price { get; set; }
+        public Product(int id, string title, string description, string imageUrl, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+        }
+
+    }
+}
